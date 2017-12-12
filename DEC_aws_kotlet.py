80f4bd63caf5d9185188691f3ff65bf46ac37a93
Ubuntu <ubuntu@ip-172-31-32-31.eu-west-1.compute.internal>
2017-12-12 15:59:45 +0000

    Best and worst

--- DEC_aws_kotlet.py
+++ DEC_aws_kotlet.py
@@ -16,19 +16,21 @@ start = time.time()
 #img_paths = glob.glob("./Kotelet/*/*.png")
 cuts_o = glob.glob("./Kotelet/Cut/*.png")
 normals_o = glob.glob("./Kotelet/Normal/*.png")
-cuts_e = glob.glob("./Kotelet_enhanced/Cut/*.png")
-normals_e = glob.glob("./Kotelet_enhanced/Normal/*.png")
+#cuts_e = glob.glob("./Kotelet_enhanced/Cut/*.png")
+#normals_e = glob.glob("./Kotelet_enhanced/Normal/*.png")
 
 img_paths_o = cuts_o + normals_o
-img_paths_e = cuts_e + normals_e
-img_paths = img_paths_o + img_paths_e
+#img_paths_e = cuts_e + normals_e
+#img_paths = img_paths_o + img_paths_e
+img_paths = img_paths_o
 
 imgs = np.asarray([resize(imread(img, as_grey=True), (60, 60), mode='constant').flatten() for img in img_paths])
 print("Shape: " + str(imgs.shape))
 
 y_o = [0 if path[10] == 'N' else 1 for path in img_paths_o]
-y_e = [0 if path[19] == 'N' else 1 for path in img_paths_e]
-y = np.asarray(y_o + y_e)
+#y_e = [0 if path[19] == 'N' else 1 for path in img_paths_e]
+#y = np.asarray(y_o + y_e)
+y = np.asarray(y_o)
 
 #n_clusters,
 #                 input_dim,
@@ -39,8 +41,8 @@ y = np.asarray(y_o + y_e)
 #                 cluster_centres=None,
 #                 batch_size=256,
 #
-c = keras_dec.DeepEmbeddingClustering(n_clusters=1, input_dim=10000, batch_size=64)
-c.initialize(X=imgs, finetune_iters=100000, layerwise_pretrain_iters=50000)
+c = keras_dec.DeepEmbeddingClustering(n_clusters=1, input_dim=3600, batch_size=64)
+c.initialize(X=imgs, finetune_iters=10000, layerwise_pretrain_iters=5000)
 outp = c.cluster(X=imgs, y=y, tol=0.01, update_interval=10000, iter_max=1000000, save_interval=10000)
 
 
